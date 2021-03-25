5946888f68b944192c87b026528f9a1fd9ff6780
Oksana <pylypenkok@gmail.com>
2021-03-25 18:47:22 +0200

    Implemented logic

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const text = document.querySelector("#text");
+  const submitBtn = document.querySelector(".submit_btn");
+  let completedText = document.querySelector("#completed_text");
+
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    completedText.value = text.value
+      .replace(/^\d+$/gim, "")
+      .replace(/^\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}$/gm, "")
+      .replace(/\n{3,}/g, "\n\n");
+  });
+});
